03520510a9749747e04ed3501738c0ddbc70b8c9
Travis Burmaster <travis.burmaster@caylent.com>
2023-09-12 15:31:15 -0600

    update

--- huggingface.py
+++ huggingface.py
@@ -18,3 +18,27 @@ llm_model = HuggingFaceModel(
     'SM_NUM_GPUS': json.dumps(number_of_gpu)
   }
 ) 
+
+prompt=f"""<|prompter|write a poem on the beauty iran country.<|endoftext|><|assistant|>"""
+# hyperparameters for llm
+payload = {
+  "inputs": prompt,
+  "parameters": {
+    "do_sample": True,
+    "top_p": 0.7,
+    "temperature": 0.7,
+    "top_k": 50,
+    "max_new_tokens": 512,
+    "repetition_penalty": 1.03,
+    "stop": ["<|endoftext|>"]
+  }
+}
+# send request to endpoint
+response = llm.predict(payload)
+runtime = boto3.client('runtime.sagemaker')
+response = runtime.invoke_endpoint(EndpointName = 'llm-endpoint',
+                                        ContentType = 'application/json',
+                                        Body = json.dumps(payload))
+output = response['Body'].read().decode()
+output_json = json.loads(output[1:-1])['generated_text'].split("<|assistant|>")[1]
+print(output_json)
